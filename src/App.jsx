d9f698c2ddebaff20fa8804915a72c15cfb528bb
truonggiang2512 <hanguyentruonggiang004@gmail.com>
2023-06-04 20:40:36 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Button from "@mui/material/Button";
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <Button variant="contained">Hello World</Button>;
+}
+
+export default App;
